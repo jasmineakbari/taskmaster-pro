5fd175b9996e10e6c801a6fa3f958eb2791c67b1
unknown <jasmineakbari@yahoo.com>
2020-11-09 20:31:08 -0500

    added functionality for task due date change and reversion

--- assets/js/script.js
+++ assets/js/script.js
@@ -83,16 +83,67 @@ $(".list-group").on("click", "p", function() {
     tasks[status][index].text = text;
     saveTasks();
 
-    //recreate p element
+    // recreate p element
     var taskP = $("<p>")
       .addClass("m-1")
       .text(text);
 
-    //replace textarea with p element
+    // replace textarea with p element
     $(this).replaceWith(taskP);
   })
 });
 
+// due date was clicked event listener to prompt edit due date
+$(".list-group").on("click", "span", function() {
+  // get current text
+  var date = $(this)
+    .text()
+    .trim();
+  
+  // create new input element
+  var dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+
+  // swap out elements
+  $(this).replaceWith(dateInput);
+
+  // automatically focus on new element
+  dateInput.trigger("focus");
+})
+
+// value of due date was changed event listener to revert upon blur
+$(".list-group").on("blur", "input[type='text']", function () {
+  // get current text
+  var date = $(this)
+    .val()
+    .trim();
+
+  // get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+  
+  // get task's podition in the list of other li elements
+
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+  
+  // update the task in array and resave to localstorage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  // recreate span element with bootstrap classes
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+
+  // replace input with span element
+  $(this).replaceWith(taskSpan);
+});
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
