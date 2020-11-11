83567c4badeadecc4e69d82fc9dd6d9fe9791b34
unknown <jasmineakbari@yahoo.com>
2020-11-10 22:33:59 -0500

    added sortable function and save updates to localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -179,6 +179,52 @@ $("#task-form-modal .btn-primary").click(function() {
   }
 });
 
+// Function to make columns sortable and save updates
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+
+  activate: function(event) {
+  }, 
+  deactivate: function(event) {
+  },
+  over: function(event) {
+  },
+  out: function(event) {
+  },
+  update: function(event) {
+    var tempArr= [];
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+
+    // trim down list's ID to match object property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+    //update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+})
+
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
